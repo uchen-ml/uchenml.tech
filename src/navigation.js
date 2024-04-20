ce5c1c62ac9af61deda4b7c2306def8ea2bd114a
Eugene Ostroukhov <eostroukhov@gmail.com>
2024-04-20 13:00:42 -0700

    Ready to write blog

--- src/navigation.js
+++ src/navigation.js
@@ -8,28 +8,7 @@ export const headerData = {
     },
     {
       text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
+      href: getBlogPermalink(),
     },
   ],
 };
@@ -38,10 +17,7 @@ export const footerData = {
   links: [
     {
       title: 'Company',
-      links: [
-        // { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-      ],
+      links: [{ text: 'Blog', href: '/blog' }],
     },
   ],
   socialLinks: [
