fbba92e2f3635c9f600313f712ebaf117f20e2d9
Eugene Ostroukhov <eostroukhov@gmail.com>
2024-04-20 11:13:12 -0700

    Completely different template

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+import typographyPlugin from '@tailwindcss/typography';
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
+  content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        body: "rgb(var(--color-bg))",
-        "box-bg": "rgb(var(--color-box))",
-        "box-shadow": "rgb(var(--box-sd))",
-        "box-border": "rgb(var(--box-border))",
-        primary: "#1d4ed8",
-        "heading-1": "rgb(var(--heading-1))",
-        "heading-2": "rgb(var(--heading-2))",
-        "heading-3": "rgb(var(--heading-3))",
+        primary: 'var(--aw-color-primary)',
+        secondary: 'var(--aw-color-secondary)',
+        accent: 'var(--aw-color-accent)',
+        default: 'var(--aw-color-text-default)',
+        muted: 'var(--aw-color-text-muted)',
       },
-      screens: {
-        midmd: "880px",
+      fontFamily: {
+        sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+        serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
+        heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typographyPlugin],
+  darkMode: 'class',
 };
