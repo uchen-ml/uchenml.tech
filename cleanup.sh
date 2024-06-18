cc387c54406c402e9d3302b485c41c699e81187f
Eugene Ostroukhov <eostroukhov@gmail.com>
2024-06-17 19:11:23 -0700

    Build

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf .astro .yarn _astro about blog category favicon.svg
+rm -rf images index.html node_modules optimizing-linear rss.xml
+rm -rf sitemap-0.xml sitemap-index.xml tag test-driven
